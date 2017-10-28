ba6d1a13d8e470858e6f97c16759052c76b329e2
ianapt <ianapthompson@gmail.com>
2017-10-28 16:23:50 -0700

    Changed the code to sample period based off new downsampled oscillator

--- syringeCode/freqCount.c
+++ syringeCode/freqCount.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 #include <errno.h>
+#include <time.h>
 #include <string.h>
 #include <stdint.h>
 
@@ -13,9 +14,11 @@
 #define HIGH 1
 #define COUNTER_PIN 0
 
+#define SAMPLING_NUMBER 10
+
 void counterInterrupt(void);
 
-volatile uint32_t counter = 0;
+volatile int timeDiff = 0;
 
 int main(void) {
 	wiringPiSetup();
@@ -23,13 +26,23 @@ int main(void) {
 	wiringPiISR(COUNTER_PIN, INT_EDGE_RISING, counterInterrupt);
 
 	while(TRUE){
-		delay(1000);
-		printf("Count is: %d\n", counter);
-		counter = 0;
+		delay(500);
+		std::cout << timeDiff << std::endl;
 	}
 	return 0;
 }
 
 void counterInterrupt(void){
+	static uint8_t	counter = 0;
+	static int oldTime = micros();
+	static int newTime;
 	counter++;
+	if(counter >= SAMPLING_NUMBER){
+		newTime = micros();
+		if(newTime > oldTime){
+			timeDiff = newTime - oldTime;
+		}
+		oldTime = newTime;
+		counter = 0;
+	}
 }
